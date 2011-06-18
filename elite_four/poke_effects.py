e1f61c259e93184fabce9b032516ae213f12d4c3
Zachary Allaun <zach.allaun@gmail.com>
2011-06-18 16:51:40 -0400

    effects template

---
+++ elite_four/poke_effects.py
@@ -0,0 +1,231 @@
+class Effect(object):
+	
+	def __init__(self, effect, effect_dict):
+		self.effect = effect
+		self.effect_dict = effect_dict
+	
+	
+
+
+class AtkUp(Effect):
+	def __init__(self):
+		effect = 'atk_up'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class Burn(Effect):
+	def __init__(self):
+		effect = 'burn'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class Confuse(Effect):
+	def __init__(self):
+		effect = 'confuse'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class Counter(Effect):
+	def __init__(self):
+		effect = 'counter'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class DefUp(Effect):
+	def __init__(self):
+		effect = 'def_up'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class DmgDown(Effect):
+	def __init__(self):
+		effect = 'dmg_down'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class FireSpinEffect(Effect):
+	def __init__(self):
+		effect = 'fire_spin_effect'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class Flinch(Effect):
+	def __init__(self):
+		effect = 'flinch'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class FlyTarget(Effect):
+	def __init__(self):
+		effect = 'fly_target'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class FlyUser(Effect):
+	def __init__(self):
+		effect = 'fly_user'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class Freeze(Effect):
+	def __init__(self):
+		effect = 'freeze'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class FutureSightEffect(Effect):
+	def __init__(self):
+		effect = 'future_sight_effect'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class HailEffect(Effect):
+	def __init__(self):
+		effect = 'hail_effect'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class HitFirst(Effect):
+	def __init__(self):
+		effect = 'hit_first'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class MustAsleep(Effect):
+	def __init__(self):
+		effect = 'must_asleep'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class OutrageEffect(Effect):
+	def __init__(self):
+		effect = 'outrage_effect'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class Paralyze(Effect):
+	def __init__(self):
+		effect = 'paralyze'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class Poison(Effect):
+	def __init__(self):
+		effect = 'poison'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class Recharge(Effect):
+	def __init__(self):
+		effect = 'recharge'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class RecoilFourth(Effect):
+	def __init__(self):
+		effect = 'recoil_25'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class RecoilThird(Effect):
+	def __init__(self):
+		effect = 'recoil_33'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class RecoverEffect(Effect):
+	def __init__(self):
+		effect = 'recover_effect'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class ReflectEffect(Effect):
+	def __init__(self):
+		effect = 'reflect_effect'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class RoarEffect(Effect):
+	def __init__(self):
+		effect = 'roar_effect'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class SafeguardEffect(Effect):
+	def __init__(self):
+		effect = 'safeguard_effect'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class Sleep(Effect):
+	def __init__(self):
+		effect = 'sleep'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class SpeedDown(Effect):
+	def __init__(self):
+		effect = 'speed_down'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
+
+
+class UserHealHalf(Effect):
+	def __init__(self):
+		effect = 'user_hp_50'
+		effect_dict = {}
+		Effect.__init__(self, effect, effect_dict)
+	
